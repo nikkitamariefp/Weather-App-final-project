5cf630aa7365c8df6130ddd9a1c2d188f3c69fbf
nikkitamariefp <137980857+nikkitamariefp@users.noreply.github.com>
2023-07-26 17:58:29 +0800

    Added apiURL for the forecast

--- app.js
+++ app.js
@@ -65,6 +65,16 @@ function handleSubmit(event) {
   search(city);
 }
 
+function getForecast(coordinates) {
+  let apiKey = "b35c686ba9565ba0ab254c2230937552";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  console.log(apiUrl);
+
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showCityTemp(response) {
   document.querySelector("#current-city").innerHTML = response.data.name;
   document.querySelector("#current-temperature").innerHTML = Math.round(
@@ -84,7 +94,7 @@ function showCityTemp(response) {
 
   celsiusTemperature = response.data.main.temp;
 
-  displayForecast();
+  getForecast(response.data.coord);
 }
 
 function displayCurrentLocation(event) {
@@ -97,6 +107,8 @@ function searchLocation(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(`${apiUrl}`).then(showCityTemp);
+
+  displayForecast();
 }
 
 function displayFahrenheitTemp(event) {
@@ -113,7 +125,8 @@ function displayCelsiusTemp(event) {
     Math.round(celsiusTemperature);
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = "";
